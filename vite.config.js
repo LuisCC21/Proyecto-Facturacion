804d87375121f6970c1f72186d9120c8ea5531f0
LuisCC21 <carcamocariasl@yahoo.com>
2024-12-09 19:16:12 -0600

    modificacion router nueva

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Proyecto-Facturacion",
+  base: "https://luiscc21.github.io/Proyecto-Facturacion",
   plugins: [react()],
 })
