cdeeb0db5d5aee533af5a49c511465e878aa31da
LuisCC21 <carcamocariasl@yahoo.com>
2024-12-09 18:30:15 -0600

    modificacion router

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -11,6 +11,7 @@ export const AppRouter = () => {
         <Route path='/factura' element={<Factura />} />
         <Route path='/facturas' element={<FacturasList />} />
 
+        <Route path='*' element={<Navigate to={'/'} />} />
         <Route path='/*' element={<Navigate to={'/'} />} />
       </Routes>
     </>
